52baf9d25599b3cce589bfb6840c2498f525263a
probably-not-porter <pelibby16@earlham.edu>
2020-09-15 22:36:14 -0400

    make some new versions

--- index.js
+++ index.js
@@ -1,26 +1,23 @@
+console.log("\n==================================");
+console.log("|           Portfolio            |");
+console.log("==================================\n");
+
 const express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-
-var jsonFile = require('jsonfile');
-var jsonData = jsonFile.readFileSync('content.json');
-console.log('--- BUILDING CONTENT TREE FROM JSON ---')
-console.log(jsonData);
-
-
 const app = express();
 
-app.use(favicon(path.join(__dirname, 'public/img_icons', 'favicon.png')));
-
 app.set('view engine', 'ejs');
-app.get('/', function(req, res) {
-    res.render("index",{
-        content: jsonData,
+app.get('/v1', function(req, res) {
+    res.render("v1",{
     })
 });
-
+app.get('/v2', function(req, res) {
+    res.render("v2",{
+    })
+});
 app.use(express.static(__dirname + '/public'));
 
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+
+// LISTEN
+app.listen(process.env.PORT || 9900, function(){
+    console.log("Server: Running on port %d in %s mode", this.address().port, app.settings.env);
 });
